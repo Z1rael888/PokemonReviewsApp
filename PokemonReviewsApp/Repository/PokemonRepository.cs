adfa108658dfa0566e1514a983b7a9a6ce45f158
Z1rael888 <alekseibaggmet8@gmail.com>
2023-06-29 14:53:05 +0200

    Add project files.

---
+++ PokemonReviewsApp/Repository/PokemonRepository.cs
@@ -0,0 +1,19 @@
+﻿using PokemonReviewsApp.Data;
+using PokemonReviewsApp.Interfaces;
+using PokemonReviewsApp.Models;
+
+namespace PokemonReviewsApp.Repository
+{
+    public class PokemonRepository : IPokemonRepository
+    {
+        private readonly DataContext _context;
+        public PokemonRepository(DataContext context)
+        {
+            _context = context; 
+        }
+        public ICollection<Pokemon> GetPokemons()
+        {
+            return _context.Pokemon.OrderBy(p=>p.Id).ToList();
+        }
+    }
+}
