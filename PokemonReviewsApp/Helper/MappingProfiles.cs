ec832c803215d7011b84cdf0420ab8eb1a46b0da
Z1rael888 <alekseibaggmet8@gmail.com>
2023-06-30 14:46:44 +0200

    Update commit,added Dto and Helper folders and Automapper as well

---
+++ PokemonReviewsApp/Helper/MappingProfiles.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using PokemonReviewsApp.Dto;
+using PokemonReviewsApp.Models;
+
+namespace PokemonReviewsApp.Helper
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Pokemon, PokemonDto>();
+        }
+    }
+}
--- PokemonReviewsApp/Repository/PokemonRepository.cs
+++ PokemonReviewsApp/Repository/PokemonRepository.cs
@@ -11,9 +11,35 @@ namespace PokemonReviewsApp.Repository
         {
             _context = context; 
         }
+
+        public Pokemon GetPokemon(int id)
+        {
+            return _context.Pokemon.Where(p => p.Id == id).FirstOrDefault();
+        }
+
+        public Pokemon GetPokemon(string name)
+        {
+            return _context.Pokemon.Where(p => p.Name == name).FirstOrDefault();//searching for name in the web site
+        }
+
+        public decimal GetPokemonRating(int pokeId)
+        {
+            var review = _context.Reviews.Where(p => p.Pokemon.Id == pokeId);
+            if (review.Count() <= 0)
+                return 0;
+            return ((decimal)review.Sum(r => r.Rating) / review.Count());
+
+        }
+
         public ICollection<Pokemon> GetPokemons()
         {
             return _context.Pokemon.OrderBy(p=>p.Id).ToList();
+        }
+
+        public bool PokemonExists(int pokeId)
+        {
+            return _context.Pokemon.Any(p => p.Id == pokeId);
+
         }
     }
 }
